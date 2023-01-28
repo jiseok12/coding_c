d0fc9b2305778fabe6ed1b767f186a37a1dac7d3
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2023-01-28 10:20:45 +0900

    8p

--- 8p/coding.c
+++ 8p/coding.c
@@ -3,15 +3,28 @@ int main() {
 	char input[30];
 	int count=0;
 	int count2=0;
+	int error=0;
 	scanf("%s",input);
-	
+
 	for(int i=0;input[i]!='\0';i++){
-		if(input[i]=='(')
+		if(input[i]=='('){
+			if(input[i+1]=='\0')
+				error=1;
 			count++;
-		else if(input[i]==')')
+		}
+		else if(input[i]==')'){
+			if(count==0){
+				error=1;
+			}
 			count2++;
+		}
+			
 	}
-	
-	printf("%s", count==count2?"YES":"NO");
+	if(error==1){
+		printf("NO");
+	}
+	else{
+		printf("%s", count==count2?"YES":"NO");
+	}
 	return 0;
 }
